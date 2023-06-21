f7dfb1118f9e4ce2e4394c7e12bc208aefd7ead0
StrotsevSV <strocev@gmail.com>
2023-06-21 15:18:23 +0300

    all task

---
+++ task64/zad64.cs
@@ -0,0 +1,43 @@
+﻿// Задача 64: 
+// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> "1, 2, 3, 4, 5"
+// M = 4; N = 8. -> "4, 5, 6, 7, 8"
+
+Console.Clear();
+Console.WriteLine($"!1! - задайте 2 переменные\n!2! - выведете все натуральные числа в диапазоне между переменных");
+
+int Prompt(string message)
+{
+    int rezult = 0;
+    try
+    {
+        Console.Write(message);
+        rezult = int.Parse(Console.ReadLine()!);
+    }
+    catch
+    {
+        Console.WriteLine("ошибка в методе Prompt");
+    }
+    return rezult;
+}
+
+int M = Prompt("Задайте переменную M - ");
+int N = Prompt("Задайте переменную N - ");
+Console.Write($"M = {M}; N = {N}. -> ");
+Math(M,N);
+
+
+void Math(int M, int N)
+{
+    if (M<=N)
+    {
+        Console.Write(M+", ");Math(M+1,N);
+    }
+}
+
+
+
+
+
+
+
---
+++ task66/zad66.cs
@@ -0,0 +1,38 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Clear();
+Console.WriteLine("!1! - Задайте значения M и N\n!2! - Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N");
+
+// прромпт 
+// создать переменные
+// создать цикл для суммирования
+
+int Prompt (string message)
+{
+    int rezult = 0;
+    try
+    {
+        Console.Write(message);
+        rezult = int.Parse(Console.ReadLine());
+    }
+    catch 
+    {
+        Prompt("Ошибка в методе Prompt, попробуйте снова: ");
+    }
+    return rezult;
+}
+
+int M = Prompt("Задайте значения M - ");
+int N = Prompt("Задайте значения N - ");
+int sum = Sum(M,N);
+Console.Write($"M = {M}; N = {N} -> {sum}");
+
+int Sum(int M, int N)
+{
+   if (M > N )
+        return 0;
+    else
+        return M + Sum(M + 1, N);
+}
